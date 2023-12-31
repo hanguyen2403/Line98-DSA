83d8eaba872f424d9285aac8a9e13ca3c37b2db3
tpbduy2303 <baoduy230303@gmail.com>
2023-12-31 23:03:23 +0700

    add group name to Link class

--- src/Lines/Link.java
+++ src/Lines/Link.java
@@ -1,3 +1,8 @@
+/*Name: Group 15
+  Nguyễn Khánh Hà - ITCSIU21004
+  Phạm Anh Huy - ITCSIU21133
+  Trần Quang Bảo Duy - ITCSIU21176
+  Purpose: This class is to apply undo feature of game*/
 package Lines;
 
 class Link {
