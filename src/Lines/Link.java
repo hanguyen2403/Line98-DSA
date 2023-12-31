7592d3224360a3023f0a1f6c2b11f8695d533fb4
tpbduy2303 <baoduy230303@gmail.com>
2023-12-31 23:01:01 +0700

    remove some unnecessary line

---
+++ src/Lines/Link.java
@@ -0,0 +1,21 @@
+package Lines;
+
+class Link {
+    public int[][] ball = new int[Constant.Row][Constant.Column];
+    public int[] color = new int[3];
+    public long score;
+    public Link next;
+    public Link previous;
+
+    public Link(int[][] b, int[] c, long s) {
+        for (int i = 0; i < Constant.Row; i++)
+            for (int j = 0; j < Constant.Column; j++)
+                ball[i][j] = b[i][j];
+
+        for (int k = 0; k < 3; k++)
+            color[k] = c[k];
+
+        score = s;
+    }
+
+}
--- src/Lines/LinkedListUndo.java
+++ src/Lines/LinkedListUndo.java
@@ -5,26 +5,6 @@
   Purpose: This class is to apply undo feature of game*/
 package Lines;
 
-class Link {
-	public int[][] ball = new int[Constant.Row][Constant.Column];
-	public int[] color = new int[3];
-	public long score;
-	public Link next;
-	public Link previous;
-
-	public Link(int[][] b, int[] c, long s) {
-        for (int i=0;i<Constant.Row;i++)
-            for (int j=0;j<Constant.Column;j++)
-            	ball[i][j]=b[i][j];
-
-        for (int k=0; k < 3; k++)
-            color[k] = c[k];
-
-        score = s;
-	}
-	
-}
-
 public class LinkedListUndo {
 	private Link first;
 	private Link last;
@@ -49,20 +29,7 @@ public class LinkedListUndo {
 		}
 		last = newLink;
 	}
-	
-	public Link deleteFirst() {
-		Link temp = first;
-		if(first.next == null) {
-			last = null;
-			first = null;
-		}
-		else {
-			first.next.previous = null;
-			first = first.next;
-		}
-		return temp;
-	}
-	
+
 	public Link deleteLast() {
 		Link temp = last;
 		if(last.previous == null) {
